d8fd9cd5973eb7e90b2add4360f4819ec919ad0e
ayobami0007 <saratayos8@gmail.com>
2024-10-25 08:24:11 +0200

    push initial setup of feature branch

---
+++ src/pages/Services.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from "../components/layouts/Footer"
+import Hero from '../components/home/Hero';
+const Services = () => {
+  return (
+    <div>
+        <Footer/>
+        <Hero/>
+    </div>
+  )
+}
+
+export default Services
