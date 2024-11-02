c4e25dbf8efab43e5e9f22b97e7d3c6bae407e9d
Developia <opiaaustin@gmail.com>
2024-11-02 14:09:09 +0100

    Merged Tolus Branch / Contact Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,25 +6,11 @@ import PopularProduct from "../components/layouts/PopularProduct.jsx";
 import Wcu from "../components/layouts/Wcu.jsx";
 import Testimonials from "../components/Testimonials.jsx";
 import Footer from "../components/layouts/Footer.jsx";
-// import Hero from '../components/home/Hero';
-// import ProductSection from '../components/home/ProductSection';
-// import WhyChooseUs from '../components/home/WhyChooseUs';
-// import WeHelp from '../components/home/WeHelp';
-// import PopularProduct from '../components/home/PopularProduct';
-// import Testimonial from '../components/home/Testimonial';
-// import BlogSection from '../components/home/BlogSection';
 
 const Home = () => {
     return (
         <>
-            {/* <h1> this is home </h1> */}
-            {/* <Hero /> */}
-            {/* <ProductSection />
-      <WhyChooseUs />
-      <WeHelp />
-      <PopularProduct />
-      <Testimonial />
-      <BlogSection /> */}
+           
             <div className="site-wrap">
                 <Header/>
                 <Hero/>
