987340963306b11263dc8c93ef38e5998cc4909f
Olayori-X <latubosun4328@gmail.com>
2025-02-11 16:24:17 +0100

    Facebook Auth

--- src/components/FacebookLogin.jsx
+++ src/components/FacebookLogin.jsx
@@ -5,17 +5,27 @@ const FacebookLogin = () => {
         window.fbAsyncInit = function () {
             window.FB.init({
                 // appId: "1133904004967612",
-                // appId: "1159221362481568",
-                appId: "934822008839118",
+                appId: "1159221362481568",
+                // appId: "934822008839118",
                 cookie: true,
                 xfbml: true,
-                version: "v18.0",
+                version: "v22.0",
             });
         };
+
+        (function (d, s, id) {
+            let js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        })(document, "script", "facebook-jssdk");
     }, []);
 
     const handleLogin = () => {
         window.FB.login((response) => {
+            console.log(response);
             if (response.authResponse) {
                 console.log("User logged in", response);
                 window.FB.api("/me", { fields: "name,email,picture" }, (userInfo) => {
